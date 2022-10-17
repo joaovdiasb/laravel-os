3d3f5572fed01035abbde21ffe89882a0ffbb65b
João Bittencourt <j.v_dias@hotmail.com>
2022-10-17 11:27:01 -0300

    chore: icone dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -8,7 +8,7 @@ use Filament\Pages\Page;
 
 class Dashboard extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-line';
 
     protected static string $view = 'filament.pages.dashboard';
 
